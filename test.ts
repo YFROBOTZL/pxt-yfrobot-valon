7a38c09a9fc37b2dfd931bb30ec533bd7b7e51eb
finalvalue <finalvalue@foxmail.com>
2020-07-21 20:46:46 +0800

    T23

--- test.ts
+++ test.ts
@@ -1,7 +1,3 @@
 // tests go here; this will not be compiled when this package is used as a library
 basic.forever(function () {
-    // valon.motorRun(ValonMotors.MAll, ValonDir.CW, 120)
-    // basic.pause(1000)
-    // valon.motorRun(ValonMotors.MAll, ValonDir.CCW, 120)
-    // basic.pause(1000)
 })
